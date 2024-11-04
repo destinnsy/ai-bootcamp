a408fa008ffdeecf1c27aaaa6e369692e43c80bd
Destin Ngeow <destin_ngeow@tech.gov.sg>
2024-11-04 11:55:44 +0800

    add pysqlite3-binary for linux systems

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from openai import OpenAI
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 
 st.title("AI Bootcamp Project Type A POC")
